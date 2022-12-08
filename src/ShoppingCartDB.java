f8393218191634aa5d1dce0fd9036cf6d85f26ea
HLDannyLim <dannylim.swe@gmail.com>
2022-12-08 19:47:44 +0800

    completed

---
+++ src/ShoppingCartDB.java
@@ -0,0 +1,175 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class ShoppingCartDB {
+    public static final String LOGIN = "login";
+    public static final String ADD = "add";
+    public static final String REMOVE = "remove";
+    public static final String LIST = "list";
+    public static final String SAVE = "save";
+    public static final String USERS = "users";
+    
+
+    public static final List<String> VALID_COMMANDS = Arrays.asList(LOGIN, ADD, REMOVE, LIST, SAVE, USERS);
+
+    private CartDBInMemory db ;
+    private String currentUser ;
+    private String baseFolder ;
+
+    public ShoppingCartDB(){
+        this.baseFolder = "db" ; // default
+        this.setup();
+        this.db = new CartDBInMemory(this.baseFolder);
+    }
+    public ShoppingCartDB(String baseFolder){
+        this.baseFolder = baseFolder ;
+        this.setup();
+        this.db = new CartDBInMemory(this.baseFolder);
+    }
+
+    public void setup(){
+        Path p = Paths.get(this.baseFolder);
+        if (Files.isDirectory(p)) {
+            // SKIP if directory already exits
+        } else {
+            try {
+            Files.createDirectory(p);
+            } catch (IOException e) {
+                System.out.println("Error :" + e.getMessage());
+            }
+        }
+    }
+
+    public void  startShell(){
+        System.out.println("Welcome to MultiUser Shopping Cart >> ");
+        Scanner sc = new Scanner(System.in);
+        String line ;
+        boolean stop = false ;
+        // sc.nextLine(); //current line until end of the line
+        // sc.next(); // word until white space
+        // String command = sc.next();
+        // String inputs = sc.nextLine();
+
+        while (!stop){
+            line = sc.nextLine();
+            line = line.trim();
+            System.out.println("=> " + line);
+            if (line.equalsIgnoreCase("exit")){
+                System.out.println("Exiting !!!");
+                stop = true ;
+            }
+            //Validate Command
+            if (!this.ValidateInput(line)){
+                System.out.println("Invalid input: ^^");
+            } else {
+                System.out.println("Processing : " + line);
+                this.ProcessInput(line);
+            }
+        }
+        sc.close();
+    }
+
+    public boolean ValidateInput(String input){
+        String[] parts = input.split(" ");
+        String command = parts[0].trim();
+        // Scanner lsc = new Scanner(input);
+        // String command = lsc.next().trim();
+        return VALID_COMMANDS.contains(command);
+    }
+
+    //Process input
+    public void ProcessInput(String input){
+        Scanner sc = new Scanner(input);
+        String command = sc.next().trim();
+
+        switch(command){
+            case LOGIN :
+                String username = sc.nextLine().trim();
+                this.LoginAction(username);
+                System.out.println("Print - current logged in user " + this.currentUser);
+                break;
+
+            case REMOVE :
+                String[] ditems = sc.nextLine().trim().split(" ");
+                this.RemoveAction(ditems);
+                break;
+
+            case ADD :
+                String[] aitems = sc.nextLine().trim().split(" ");
+                this.AddAction(aitems);
+                break;
+
+            case LIST :
+                this.ListAction();
+                break;
+
+            case SAVE :
+            this.SaveAction();
+                break;
+
+            case USERS :
+            this.ListUsersAction();
+                break;
+
+            default:
+                break;
+        }
+        sc.close();
+    }
+
+    public void LoginAction(String username){
+         if (!this.db.userMap.containsKey(username)){
+            this.db.userMap.put(username, new ArrayList<String>());
+         }
+         this.currentUser = username ;
+    }
+
+    public void AddAction(String[] items){
+    for(String item:items){
+    this.db.userMap.get(this.currentUser).add(item.trim());
+   }
+}
+
+    public void RemoveAction(String[] items){
+        for(String item:items){
+        this.db.userMap.get(this.currentUser).remove(item.trim());
+    }
+    }
+
+    public void ListAction(){
+        for(String item: this.db.userMap.get(this.currentUser)){
+            System.out.println(("Item -> " + item));
+        }
+        }
+    
+    public void ListUsersAction() {
+        for (String key: this.db.userMap.keySet()) {
+                System.out.println("-> " + key);
+        }
+        }
+
+    public void SaveAction() {        
+        // Prepare the filePath = "db/<username>.db"
+        String outputFilename = String.format("%s/%s.db", 
+        this.baseFolder, this.currentUser);
+
+        try {
+            FileWriter fw = new FileWriter(outputFilename);
+            // Save the contents for this user in Map to a file.
+            for (String item : this.db.userMap.get(this.currentUser)) {
+                fw.write(item +"\n");
+            }
+            fw.flush();
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
